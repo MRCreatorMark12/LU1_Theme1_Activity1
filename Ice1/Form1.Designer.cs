a9e391332317ef04eefe8e11957abfd1ff545f42
CaptainBelt <64750055+CaptainBelt@users.noreply.github.com>
2022-03-24 08:27:39 +0200

    Add project files.

---
+++ Ice1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Ice1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.lbl_Answer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btn_Submit.Location = new System.Drawing.Point(0, 410);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(800, 40);
+            this.btn_Submit.TabIndex = 0;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // lbl_Answer
+            // 
+            this.lbl_Answer.AutoSize = true;
+            this.lbl_Answer.Location = new System.Drawing.Point(350, 27);
+            this.lbl_Answer.Name = "lbl_Answer";
+            this.lbl_Answer.Size = new System.Drawing.Size(46, 17);
+            this.lbl_Answer.TabIndex = 1;
+            this.lbl_Answer.Text = "label1";
+            this.lbl_Answer.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_Answer);
+            this.Controls.Add(this.btn_Submit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.Label lbl_Answer;
+    }
+}
+
